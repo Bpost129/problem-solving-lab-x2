1a233e2b71810e2279e3a1cdd65f5fbb63048a23
Bpost129 <Bpost129@yahoo.com>
2025-10-29 14:54:54 -0400

    complete prompt 3

--- app.js
+++ app.js
@@ -11,3 +11,30 @@ function reverseWordOrder(str) {
 }
 
 console.log(reverseWordOrder("Hey there loser"))
+
+
+function calculate(num1, num2, operation) {
+  switch (operation) {
+    case 'add':
+      return num1 + num2
+    case 'sub':
+      return num1 - num2
+    case 'mult':
+      return num1 * num2
+    case 'div':
+      return num1 / num2
+    case 'exp':
+      let total = num1 * num1
+      if (num2 > 2) {
+        for (let i = 2; i < num2; i++) {
+          total *= num1
+        }
+      }
+      return total
+    default:
+      console.log('Thats not an operation')
+  }
+}
+
+console.log(calculate(4, 3, 'sub'))
+console.log(calculate(4, 3, 'exp'))
